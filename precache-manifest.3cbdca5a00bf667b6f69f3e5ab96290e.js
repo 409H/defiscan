892ea7a03984a629dc1259dd810adb9f5c479732
409H <harry.denley@outlook.com>
2019-07-11 20:27:35 +0100

    Release

---
+++ precache-manifest.3cbdca5a00bf667b6f69f3e5ab96290e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e91abe3f034c75b81749b974bd37d0a",
+    "url": "/static/media/metamask.2e91abe3.png"
+  },
+  {
+    "revision": "7aa9ec4b79632e7e4a60",
+    "url": "/static/css/main.62ffb495.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "568fd097cb9a5c205b9b",
+    "url": "/static/js/2.41101359.chunk.js"
+  },
+  {
+    "revision": "b340a6a309d8b354116a022409260f47",
+    "url": "/static/media/compound.b340a6a3.png"
+  },
+  {
+    "revision": "bbff56efa3cc4aef2ddb45ba423a7503",
+    "url": "/static/media/spankBankChainsaw.bbff56ef.svg"
+  },
+  {
+    "revision": "9f2bd08102d4c0248c348e2fb6f0f96f",
+    "url": "/static/media/uniswap.9f2bd081.png"
+  },
+  {
+    "revision": "4be28b77f97a95139dec319757071284",
+    "url": "/static/media/mycrypto.4be28b77.jpg"
+  },
+  {
+    "revision": "0eb9d45e6a8af6074e66b1858d17a746",
+    "url": "/static/media/exit.0eb9d45e.svg"
+  },
+  {
+    "revision": "f92975414cd35fdc35593600f655385e",
+    "url": "/static/media/weth.f9297541.png"
+  },
+  {
+    "revision": "7aa9ec4b79632e7e4a60",
+    "url": "/static/js/main.6c1ad6e7.chunk.js"
+  },
+  {
+    "revision": "49d8ab1c9b5f4ccdc85196f3fcab618d",
+    "url": "/static/media/coinbase.49d8ab1c.jpg"
+  },
+  {
+    "revision": "dd998e961ebf9200b4a0498a73309e16",
+    "url": "/static/media/status.dd998e96.png"
+  },
+  {
+    "revision": "520aedc3e3f48040f70351e42eaaaf2a",
+    "url": "/static/media/built-on-ethereum.520aedc3.svg"
+  },
+  {
+    "revision": "60b21bbd3ca05ad71224d8dbd08da54c",
+    "url": "/static/media/defipulse.60b21bbd.svg"
+  },
+  {
+    "revision": "c3fbbeecb88208f14443bb8233f875bf",
+    "url": "/static/media/spankchain.c3fbbeec.svg"
+  },
+  {
+    "revision": "9174f0c62f43d4a058cb30e42812e245",
+    "url": "/static/media/aragon.9174f0c6.png"
+  },
+  {
+    "revision": "c8fdb4fea2975843cf18622210b40b65",
+    "url": "/static/media/spankchain.c8fdb4fe.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e28b4db1ae625829cc1ec238759e0307",
+    "url": "/index.html"
+  }
+];
